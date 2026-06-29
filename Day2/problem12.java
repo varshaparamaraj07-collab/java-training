ba0574106af2bfcc2e676901ac90e2b6bad4fbe3
Varsha <varshaparamaraj07@gmail.com>
2026-06-29 22:45:29 +0530

    Initial

---
+++ Day2/problem12.java
@@ -0,0 +1,10 @@
+package Day2;
+
+public class problem12 {
+    public static void main(String[] args) {
+        System.out.println(Byte.SIZE);
+        System.out.println(Short.SIZE);
+        System.out.println(Integer.SIZE);
+        System.out.println(Long.SIZE);
+    }
+}
