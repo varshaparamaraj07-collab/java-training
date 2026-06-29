0efe5c099bdbf71a87ba0d2b2796dc77ee87cf13
Varsha <varshaparamaraj07@gmail.com>
2026-06-29 22:40:56 +0530

    Initial

---
+++ Day2/problem9.java
@@ -0,0 +1,14 @@
+package Day2;
+
+public class problem9 {
+    public static void main(String[] args) {
+        int a = 5, b = 10, temp;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
