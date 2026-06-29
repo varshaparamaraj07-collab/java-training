230c9509cb10f883da68d4f968591836a603e90c
Varsha <varshaparamaraj07@gmail.com>
2026-06-29 22:47:42 +0530

    Initial

---
+++ Day2/problem13.java
@@ -0,0 +1,15 @@
+package Day2;
+
+public class problem13 {
+    public static void main(String[] args) {
+        String name = "Harini";
+        int age = 20;
+        char grade = 'A';
+        float cgpa = 8.5f;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+        System.out.println("CGPA: " + cgpa);
+    }
+}
