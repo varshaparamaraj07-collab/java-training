060da0bf46376db75d6b4dfbb4c30e57610fc4ce
Varsha <varshaparamaraj07@gmail.com>
2026-06-29 22:42:49 +0530

    Initial

---
+++ Day2/problem10.java
@@ -0,0 +1,10 @@
+package Day2;
+
+public class problem10 {
+    public static void main(String[] args) {
+        double num = 10.8;
+        int value = (int) num;
+
+        System.out.println(value);
+    }
+}
