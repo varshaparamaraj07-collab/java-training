a44d1acb1296a5edb615998c06b74d2be7e23db6
Varsha <varshaparamaraj07@gmail.com>
2026-06-29 22:37:17 +0530

    Initial

---
+++ Day2/problem7.java
@@ -0,0 +1,19 @@
+package Day2;
+
+public class problem7 {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 5.5f;
+        double c = 12.34;
+        char d = 'H';
+        boolean e = true;
+        String f = "Java";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
