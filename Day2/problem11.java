ec1435a979354d107f8ee5a7bd7d00fe885b58dd
Varsha <varshaparamaraj07@gmail.com>
2026-06-29 22:43:56 +0530

    Initial

---
+++ Day2/problem11.java
@@ -0,0 +1,10 @@
+package Day2;
+
+public class problem11 {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int ascii = ch;
+
+        System.out.println(ascii);
+    }
+}
