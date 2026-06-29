90d0289056cef7df9ee70e6d33b10da329ac7934
Varsha <varshaparamaraj07@gmail.com>
2026-06-29 22:38:49 +0530

    Initial

---
+++ Day2/problem8.java
@@ -0,0 +1,9 @@
+package Day2;
+
+public class problem8 {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
