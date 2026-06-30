c873d4177321aa9855b05f5a302b0dcfcafdc082
Varsha <varshaparamaraj07@gmail.com>
2026-06-30 14:00:05 +0530

    Initial

---
+++ Day3/problem2.java
@@ -0,0 +1,11 @@
+package Day3;
+
+public class problem2 {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+}
