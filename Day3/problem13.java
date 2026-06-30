3d0521a9cc044f1adcf5d6ce76e3b77f8c37355c
Varsha <varshaparamaraj07@gmail.com>
2026-06-30 14:58:47 +0530

    Initial

---
+++ Day3/problem13.java
@@ -0,0 +1,9 @@
+package Day3;
+
+public class problem13 {
+    public static void main(String[] args) {
+        int l = 8, b = 4;
+        int area = l * b;
+        System.out.println(area);
+    }
+}
