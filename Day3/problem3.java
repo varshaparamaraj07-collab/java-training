b04050040d4c4ced7fcd14bcb5ec0e7ec02dfdcc
Varsha <varshaparamaraj07@gmail.com>
2026-06-30 14:22:39 +0530

    Initial

---
+++ Day3/problem3.java
@@ -0,0 +1,10 @@
+package Day3;
+
+public class problem3 {
+    public static void main(String[] args) {
+        boolean a = true, b = false;
+        System.out.println(a && b);
+        System.out.println(a || b);
+        System.out.println(!a);
+    }
+}
