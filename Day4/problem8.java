8c0c6931a5d2dca73d02298ad5861891c874676a
Varsha <varshaparamaraj07@gmail.com>
2026-06-30 15:32:56 +0530

    Initial

---
+++ Day4/problem8.java
@@ -0,0 +1,10 @@
+package Day4;
+
+public class problem8 {
+    public static void main(String[] args) {
+        int num = 120;
+        byte b = (byte) num;
+
+        System.out.println(b);
+    }
+}
