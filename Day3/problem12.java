1b976683ef17e81b4dd86c4d2eb92c27a957a38e
Varsha <varshaparamaraj07@gmail.com>
2026-06-30 14:57:30 +0530

    Initial

---
+++ Day3/problem12.java
@@ -0,0 +1,9 @@
+package Day3;
+
+public class problem12 {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int max = (a > b) ? a : b;
+        System.out.println(max);
+    }
+}
