ab4e9a82bf46fccd4f68cabffb61ce1eab1e34e1
Varsha <varshaparamaraj07@gmail.com>
2026-06-30 15:30:46 +0530

    Initial

---
+++ Day4/problem6.java
@@ -0,0 +1,10 @@
+package Day4;
+
+public class problem6 {
+    public static void main(String[] args) {
+        double d = 45.78;
+        int i = (int) d;
+
+        System.out.println(i);
+    }
+}
