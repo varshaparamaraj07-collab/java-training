580547789b34684c8ae080d788e96113db457398
Varsha <varshaparamaraj07@gmail.com>
2026-06-30 15:05:19 +0530

    Initial

---
+++ Day4/problem2.java
@@ -0,0 +1,10 @@
+package Day4;
+
+public class problem2 {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int value = ch;
+
+        System.out.println(value);
+    }
+}
