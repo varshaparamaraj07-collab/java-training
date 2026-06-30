ce6c51558c60d73fab6178dc0a601e79aeb3b9c1
Varsha <varshaparamaraj07@gmail.com>
2026-06-30 15:34:00 +0530

    Initial

---
+++ Day4/probleem9.java
@@ -0,0 +1,10 @@
+package Day4;
+
+public class probleem9 {
+    public static void main(String[] args) {
+        double d = 25.67;
+        float f = (float) d;
+
+        System.out.println(f);
+    }
+}
