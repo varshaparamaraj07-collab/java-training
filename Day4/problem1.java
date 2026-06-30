9c44c24d26a34b1bda44ab2ca8618e1705424cd9
Varsha <varshaparamaraj07@gmail.com>
2026-06-30 15:04:14 +0530

    Initial

---
+++ Day4/problem1.java
@@ -0,0 +1,11 @@
+package Day4;
+
+public class problem1 {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = a;
+
+        System.out.println("Integer: " + a);
+        System.out.println("Double: " + b);
+    }
+}
