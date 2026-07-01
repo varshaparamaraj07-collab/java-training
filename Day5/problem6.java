d9835169b87a366ac2e9e481e8cf123ac8c13ade
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 19:38:18 +0530

    Initial

---
+++ Day5/problem6.java
@@ -0,0 +1,12 @@
+package Day5;
+import java.util.Scanner;
+public class problem6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+
+        System.out.println("Character = " + ch);
+    }
+}
