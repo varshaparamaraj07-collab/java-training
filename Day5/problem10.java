41456016a29184ba8517e3710fbf178f45d74ad7
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 19:46:32 +0530

    Initial

---
+++ Day5/problem10.java
@@ -0,0 +1,11 @@
+package Day5;
+import java.util.Scanner;
+public class problem10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println("Square = " + (n * n));
+    }
+}
