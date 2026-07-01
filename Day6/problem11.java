d92767dfa6f6f2ce8170dae77b7e4366147d2524
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 21:47:28 +0530

    Initial

---
+++ Day6/problem11.java
@@ -0,0 +1,12 @@
+package Day6;
+
+public class problem11 {
+    public static void main(String[] args) {
+        int mark = 40;
+
+        if (mark >= 35)
+            System.out.println("Pass");
+        else
+            System.out.println("Fail");
+    }
+}
