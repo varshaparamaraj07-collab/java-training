257eca867168da2c59aadb2e1eb0b8e61013b045
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 14:04:30 +0530

    Initial

---
+++ Day5/Addition.java
@@ -0,0 +1,15 @@
+package Day5;
+import java.util.Scanner;
+public class Addition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Sum = " + (a + b));
+    }
+}
