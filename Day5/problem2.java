7c37f230e2cf6c24568e4e363dddf13a944de39a
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 14:09:20 +0530

    Initial

--- Day5/problem2.java
+++ Day5/problem2.java
@@ -1,6 +1,6 @@
 package Day5;
-import java.util.Scanner;
-public class Addition {
+ import java.util.Scanner;
+public class problem2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
