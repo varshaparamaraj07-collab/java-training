261fdd3c3b60050c3c4b4033b54d8aefefc5bf2a
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 21:45:29 +0530

    Initial

---
+++ Day6/problem10.java
@@ -0,0 +1,17 @@
+package Day6;
+
+public class problem10 {
+    public static void main(String[] args) {
+        int mark = 82;
+
+        if (mark >= 90)
+            System.out.println("A");
+        else if (mark >= 80)
+            System.out.println("B");
+        else if (mark >= 70)
+            System.out.println("C");
+        else
+            System.out.println("Fail");
+    }
+}
+
