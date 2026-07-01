7ef2ab7c1a346e838adbf3f1dbe6dfa1432255f9
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 23:06:27 +0530

    Initial

---
+++ Day8/problem13.java
@@ -0,0 +1,21 @@
+package Day8;
+
+public class problem13 {
+    public static void main(String[] args) {
+        int[][] a = {{1,2},{3,4}};
+        int[][] b = {{5,6},{7,8}};
+        int[][] c = new int[2][2];
+
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                c[i][j] = a[i][j] + b[i][j];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++)
+                System.out.print(c[i][j] + " ");
+
+            System.out.println();
+        }
+    }
+}
+
