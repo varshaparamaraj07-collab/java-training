1ac0f3e59a129589eae76f7d87b08db591179d6e
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 13:40:48 +0530

    Initial

---
+++ Day4/problem10.java
@@ -0,0 +1,10 @@
+package Day4;
+
+public class problem10 {
+    public static void main(String[] args) {
+        int num = 66;
+        char ch = (char) num;
+
+        System.out.println(ch);
+    }
+}
