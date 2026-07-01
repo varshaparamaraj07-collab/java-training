fb8ba1c75b613f9e956a132054412a78d068a97f
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 22:08:35 +0530

    Initial

---
+++ Day7/problem5.java
@@ -0,0 +1,11 @@
+package Day7;
+
+public class problem5 {
+    public class Table {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i = 1; i <= 10; i++)
+            System.out.println(n + " x " + i + " = " + (n * i));
+    }
+}
