b7a0c4d55e8e0a5a7b07d6c1b6405c255c644b06
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 21:39:01 +0530

    Initial

---
+++ Day6/problem7.java
@@ -0,0 +1,12 @@
+package Day6;
+
+public class problem7 {
+    public static void main(String[] args) {
+        int num = 55;
+
+        if (num % 5 == 0 && num % 11 == 0)
+            System.out.println("Divisible");
+        else
+            System.out.println("Not Divisible");
+    }
+}
