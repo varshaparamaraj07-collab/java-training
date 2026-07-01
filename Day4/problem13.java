1dca735b0792b3c9ee8334ec0aade7acadc92fb3
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 13:46:03 +0530

    Initial

---
+++ Day4/problem13.java
@@ -0,0 +1,12 @@
+package Day4;
+
+public class problem13 {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        double avg = (double)(a + b) / 2;
+
+        System.out.println(avg);
+    }
+}
+
