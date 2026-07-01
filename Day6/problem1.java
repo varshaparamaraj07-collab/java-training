9a658cb0c62abaae9a64186c8a2088bb165d493b
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 21:23:22 +0530

    Initial

---
+++ Day6/problem1.java
@@ -0,0 +1,12 @@
+package Day6;
+public class problem1 {
+    public static void main(String[] args) {
+        int num = 8;
+
+        if (num % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
+
