cec9c347ff239878fc9b7bf897a4898059846824
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 19:44:51 +0530

    Initial

---
+++ Day5/problem9.java
@@ -0,0 +1,17 @@
+package Day5;
+import java.util.Scanner;
+public class problem9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
