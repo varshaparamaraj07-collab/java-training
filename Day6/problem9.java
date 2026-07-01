3d8db5f821e3637efe9fb7f584f37548b9fb63c6
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 21:43:43 +0530

    Initial

---
+++ Day6/problem9.java
@@ -0,0 +1,14 @@
+package Day6;
+
+public class problem9 {
+    public static void main(String[] args) {
+        char ch = 'e';
+
+        if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
+            ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+            System.out.println("Vowel");
+        else
+            System.out.println("Consonant");
+    }
+}
+
