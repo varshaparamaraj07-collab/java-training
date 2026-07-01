aefee1b8846641f661401b971c34071e8be54cdd
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 22:42:43 +0530

    Initial

---
+++ Day7/problem13.java
@@ -0,0 +1,13 @@
+package Day7;
+
+public class problem13 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= i; j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+    }
+}
+
