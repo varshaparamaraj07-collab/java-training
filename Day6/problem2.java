ffa6eeeb0fcf2c7f8fdff7a866e73141c72a7500
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 21:24:36 +0530

    Initial

---
+++ Day6/problem2.java
@@ -0,0 +1,14 @@
+package Day6;
+
+public class problem2 {
+    public static void main(String[] args) {
+        int num = -5;
+
+        if (num > 0)
+            System.out.println("Positive");
+        else if (num < 0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+    }
+}
