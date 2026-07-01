79146fdb46438b181fd7bc15439acc91ff0cc6ca
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 21:51:21 +0530

    Initial

---
+++ Day6/problem13.java
@@ -0,0 +1,13 @@
+package Day6;
+
+public class problem13 {
+    public static void main(String[] args) {
+        int num = 18;
+
+        if (num % 3 == 0)
+            System.out.println("Multiple of 3");
+        else
+            System.out.println("Not Multiple of 3");
+    }
+}
+
