06a7a314efb76f18a41b5c42cbdd88e99a216270
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 19:51:51 +0530

    Initial

---
+++ Day5/problem13.java
@@ -0,0 +1,11 @@
+package Day5;
+import java.util.Scanner;
+public class problem13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char ch = sc.next().charAt(0);
+
+        System.out.println("ASCII Value = " + (int) ch);
+    }
+}
