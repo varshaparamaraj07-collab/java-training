8c35bde68ef20c2e1cb99e37f6a3658fc88836ce
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 21:30:06 +0530

    Initial

---
+++ Day6/problem4.java
@@ -0,0 +1,12 @@
+package Day6;
+
+public class problem4 {
+    public static void main(String[] args) {
+        int a = 20, b = 15;
+
+        if (a > b)
+            System.out.println(a);
+        else
+            System.out.println(b);
+    }
+}
