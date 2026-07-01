b5ea22afdfa3f5fc16085a8051cc427ca3779e4c
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 19:41:17 +0530

    Initial

---
+++ Day5/problem7.java
@@ -0,0 +1,13 @@
+package Day5;
+import java.util.Scanner;
+public class problem7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter float value: ");
+        float num = sc.nextFloat();
+
+        System.out.println("You entered: " + num);
+    }
+}
+
