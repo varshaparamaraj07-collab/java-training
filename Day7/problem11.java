90d73bf7e23c852927616914012f7755ff52dfc5
Varsha <varshaparamaraj07@gmail.com>
2026-07-01 22:31:44 +0530

    Initial

--- Day7/problem11.java
+++ Day7/problem11.java
@@ -2,13 +2,17 @@ package Day7;
 
 public class problem11 {
     public static void main(String[] args) {
-        int a = 0, b = 1;
+        int n = 17;
+        int count = 0;
 
-        for(int i = 1; i <= 10; i++) {
-            System.out.print(a + " ");
-            int c = a + b;
-            a = b;
-            b = c;
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0)
+                count++;
         }
+
+        if(count == 2)
+            System.out.println("Prime");
+        else
+            System.out.println("Not Prime");
     }
 }
