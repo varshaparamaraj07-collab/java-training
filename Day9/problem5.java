1943faaf440ab7a1d3c2e6581c299496dcb416a9
Varsha <varshaparamaraj07@gmail.com>
2026-07-02 14:25:27 +0530

    Initial

---
+++ Day9/problem5.java
@@ -0,0 +1,12 @@
+package Day9;
+
+public class problem5 {
+    static final double PI = 3.14159;
+
+    public static void main(String[] args) {
+        double radius = 5;
+        double area = PI * radius * radius;
+
+        System.out.println(area);
+    }
+}
