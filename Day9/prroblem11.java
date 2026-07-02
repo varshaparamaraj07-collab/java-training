819e22680412c609c464ee0c94b89b7f7b44676a
Varsha <varshaparamaraj07@gmail.com>
2026-07-02 14:40:06 +0530

    Initial

---
+++ Day9/prroblem11.java
@@ -0,0 +1,17 @@
+package Day9;
+
+public class prroblem11 {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(10, 5));
+        System.out.println(subtract(10, 5));
+    }
+}
+
