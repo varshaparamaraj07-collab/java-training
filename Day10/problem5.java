c42c8906339f9a9546a82ec0c421e338dbb8a57f
Varsha <varshaparamaraj07@gmail.com>
2026-07-02 15:10:56 +0530

    Initial

---
+++ Day10/problem5.java
@@ -0,0 +1,26 @@
+package Day10;
+
+public class problem5 {
+
+    public static void main(String[] args) {
+
+        Employee e = new Employee();
+
+        e.setId(101);
+
+        System.out.println(e.getId());
+    }
+}
+
+class Employee {
+    private int id;
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    int getId() {
+        return id;
+    }
+}
+
