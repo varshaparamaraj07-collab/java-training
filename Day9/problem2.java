81b9f1d49df9ab1c143b71a399f1132a8e7bff9a
Varsha <varshaparamaraj07@gmail.com>
2026-07-02 14:21:43 +0530

    Initial

---
+++ Day9/problem2.java
@@ -0,0 +1,11 @@
+package Day9;
+
+public class problem2 {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(10, 20));
+    }
+}
