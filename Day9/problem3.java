8f281f6d8e9b88f17b9be1bae678a6317a1a6811
Varsha <varshaparamaraj07@gmail.com>
2026-07-02 14:23:14 +0530

    Initial

---
+++ Day9/problem3.java
@@ -0,0 +1,11 @@
+package Day9;
+
+public class problem3 {
+    static int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(max(15, 25));
+    }
+}
