275da32a6219c2996187e22716ee2aa8375a089c
Varsha <varshaparamaraj07@gmail.com>
2026-07-02 15:03:41 +0530

    Initial

---
+++ Day10/problem4.java
@@ -0,0 +1,15 @@
+package Day10;
+
+public class problem4 {
+    public static void main(String[] args) {
+
+        Demo.show();
+    }
+}
+
+class Demo {
+
+    static void show() {
+        System.out.println("Hello");
+    }
+}
