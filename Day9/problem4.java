d8485e083808792525d0bb30f628caae7e61c997
Varsha <varshaparamaraj07@gmail.com>
2026-07-02 14:24:19 +0530

    Initial

---
+++ Day9/problem4.java
@@ -0,0 +1,11 @@
+package Day9;
+
+public class problem4 {
+    static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isEven(8));
+    }
+}
