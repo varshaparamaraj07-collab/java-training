56ccf8d8ce6fba5415013d1cc344acb589e12a02
Varsha <varshaparamaraj07@gmail.com>
2026-07-03 11:52:01 +0530

    Initial

---
+++ Day10/problem8.java
@@ -0,0 +1,26 @@
+package Day10;
+
+public class problem8 {
+    public static void main(String[] args) {
+
+        Access a = new Access();
+
+        a.show();
+    }
+}
+
+class Access {
+
+    public int a = 10;
+    protected int b = 20;
+    int c = 30;
+    private int d = 40;
+
+    void show() {
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
