69cb2b8966315ac1261478e5385bbacef18783bb
Varsha <varshaparamaraj07@gmail.com>
2026-07-15 09:13:16 +0530

    Initial

---
+++ Day22/problem2.java
@@ -0,0 +1,27 @@
+package Day22;
+
+public class problem2 {
+    class Solution {
+    public boolean backspaceCompare(String s, String t) {
+
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c != '#')
+                sb1.append(c);
+            else if (sb1.length() > 0)
+                sb1.deleteCharAt(sb1.length() - 1);
+        }
+
+        for (char c : t.toCharArray()) {
+            if (c != '#')
+                sb2.append(c);
+            else if (sb2.length() > 0)
+                sb2.deleteCharAt(sb2.length() - 1);
+        }
+
+        return sb1.toString().equals(sb2.toString());
+    }
+}
+    
---
+++ Day22/problem3.java
@@ -0,0 +1,26 @@
+package Day22;
+
+public class problem3 {
+    class Solution {
+    public int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int max = 0;
+
+        while (left < right) {
+
+            int area = Math.min(height[left], height[right]) * (right - left);
+            max = Math.max(max, area);
+
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+
+        return max;
+    }
+}
+    
+
