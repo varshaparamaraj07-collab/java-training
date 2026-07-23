379305053430b3cfdcb3c71da9b81195e4be4205
Varsha <varshaparamaraj07@gmail.com>
2026-07-23 17:36:37 +0530

    Initial

---
+++ Day29/problem6.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++)
+            graph.add(new ArrayList<>());
+
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+
+        boolean[] visited = new boolean[n];
+
+        return dfs(graph, visited, source, destination);
+    }
+
+    private boolean dfs(List<List<Integer>> graph, boolean[] visited,
+                        int node, int destination) {
+
+        if (node == destination)
+            return true;
+
+        visited[node] = true;
+
+        for (int next : graph.get(node)) {
+            if (!visited[next]) {
+                if (dfs(graph, visited, next, destination))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+}
